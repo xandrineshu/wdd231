010ebec1479bf076ff0b083209f207da94cbde5b
xandrineshu <jconcha1@byupathway.edu>
2025-09-01 19:41:04 +0800

    w01 learning activity

--- scripts/melodyhub.js
+++ scripts/melodyhub.js
@@ -1,5 +1,7 @@
 const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
 
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
