11d5fa9d0d116bd5e01136704f30be96721b0a2f
xandrineshu <jconcha1@byupathway.edu>
2025-09-08 17:48:19 +0800

    json activity

---
+++ scripts/json.js
@@ -0,0 +1,31 @@
+{
+    "family_name": "Concha",
+    "move_in_date": "2020-02-16",
+    "number_of_people": 4,
+    "visited_by_bishopric": true,
+    "family_members": [
+        {
+            "name": "Virgilio",
+            "age": 50,
+            "birthdate": "1973-01-27"
+        },
+
+        {
+            "name": "Jennifer",
+            "age": 51,
+            "birthdate": "1974-03-17"
+        },
+
+        {
+            "name": "Jelina Alexandrine",
+            "age": 21,
+            "birthdate": "2003-12-18"
+        },
+
+        {
+            "name": "Alisson Eve",
+            "age": 19,
+            "birthdate": "2006-09-07"
+        }
+    ]
+}
