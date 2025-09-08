f6d980ad958fc5600b5dc88883c7e3a0f5c2b00a
xandrineshu <jconcha1@byupathway.edu>
2025-09-08 18:52:47 +0800

    prophets activity

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const getProphetData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+};
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+        let portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date Of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place Of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Retrato de ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '350');
+        portrait.setAttribute('height', '450');
+
+        card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+};
+
+getProphetData();
