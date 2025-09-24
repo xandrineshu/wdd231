b57ee86c313734018e5c3801559d5aafe848f63f
xandrineshu <jconcha1@byupathway.edu>
2025-09-24 19:23:38 +0800

    fix timestamp problem

--- chamber/scripts/results.js
+++ chamber/scripts/results.js
@@ -1,34 +1,25 @@
 // Confirm Membership (Results)
 
 const myInfo = new URLSearchParams(window.location.search);
-// console.log(myInfo);
 
-function setTimestamp() {
+let timestamp = myInfo.get("timestamp");
+if (!timestamp) {
     const currentDate = new Date();
-    const timestamp = currentDate.toLocaleString();
-    document.getElementById('timestamp').value = timestamp;
-    console.log(timestamp);
-    return true;
+    timestamp = currentDate.toLocaleString();
 }
 
-// console.log(myInfo.get("firstName"));
-// console.log(myInfo.get("lastName"));
-// console.log(myInfo.get("email"));
-// console.log(myInfo.get("tel"));
-// console.log(myInfo.get("orgName"));
-// console.log(myInfo.get("membership"));
-// console.log(myInfo.get("timestamp"));
-
 document.querySelector("#results").innerHTML = `
-    <p><strong>${myInfo.get("firstName")} ${myInfo.get("lastName")}</strong>, Thank you for completing the application form for the
-    Philippines Chamber of Commerce Membership, your application will be reviewed and you will be contacted shortly.
-    Our team will review your application and get back to you shortly. In the meantime, feel free to explore our page! ^^
+    <p>
+        <strong>${myInfo.get("firstName") || ""} ${myInfo.get("lastName") || ""}</strong>, thank you for completing the application form for the
+        Philippines Chamber of Commerce Membership! Your application will be reviewed and you will be contacted shortly through email.
+        In the meantime, feel free to explore our page and what we offer! Have a good day 😊
     </p>
     
     <h2><strong>Application details:</strong></h2>
-    <p><strong>Business/Organization Name:</strong> ${myInfo.get("orgName")}</p>
-    <p><strong>Email Address:</strong> ${myInfo.get("email")}</p>
-    <p><strong>Phone Number:</strong> ${myInfo.get("tel")}</p>
-    <p><strong>Membership Level Acquired:</strong> ${myInfo.get("membership")}</p>
-    <p><strong>Application Submitted:</strong> ${myInfo.get("timestamp")}</p>
+    <ul>
+        <li><p><strong>Business/Organization Name:</strong> ${myInfo.get("orgName") || ""}</p></li>
+        <li><p><strong>Email Address:</strong> ${myInfo.get("email") || ""}</p></li>
+        <li><p><strong>Phone Number:</strong> ${myInfo.get("tel") || ""}</p></li>
+        <li><p><strong>Membership Level Acquired:</strong> ${myInfo.get("membership") || ""}</p></li>
+        <li><p><strong>Application Submitted:</strong> ${timestamp}</p></li>
 `;
