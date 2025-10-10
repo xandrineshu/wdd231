2dd51bc57c59b4f199c82aa311c59a8a5b260474
xandrineshu <jconcha1@byupathway.edu>
2025-10-10 23:20:26 +0800

    create youtuber data

---
+++ finalproject/data/youtubers.mjs
@@ -0,0 +1,66 @@
+export const youtubers = [
+    {
+        "name": "KubzScouts",
+        "description": "Philippine-based YouTuber known for energetic horror playthroughs, jump scares, and late-night 'Free Random Horror Game' sessions.",
+        "channel_link": "https://www.youtube.com/channel/UCZBY6V8Lxmwu8gGRBOyO11w",
+        "image_url": "images/KubzScouts.webp",
+        "famous_horror_games_played": [
+            "Free Random Horror Games", 
+            "Silent Hill f", 
+            "The Walking Dead", 
+            "Yandere Simulator"
+        ]
+    },
+
+    {
+        "name": "Markiplier",
+        "description": "One of the most iconic horror gaming YouTubers, known for dramatic reactions, immersive commentary, and playthroughs of both indie and AAA horror titles.",
+        "channel_link": "https://www.youtube.com/user/markiplierGAME",
+        "image_url": "images/Markiplier.webp",
+        "famous_horror_games_played": [
+            "Amnesia: The Dark Descent",
+            "Five Nights at Freddy's series",
+            "Outlast",
+            "Resident Evil 7: Biohazard"
+        ]
+    },
+
+    {
+        "name": "CoryxKenshin",
+        "description": "Popular gaming YouTuber celebrated for his comedic horror playthroughs and his long-running 'Spooky Scary Sunday' horror series.",
+        "channel_link": "https://www.youtube.com/c/CoryxKenshin",
+        "image_url": "images/CoryxKenshin.webp",
+        "famous_horror_games_played": [
+            "Murder House", 
+            "The Mortuary Assistant",
+            "Stay Out of the House", 
+            "MADiSON"
+        ]
+    },
+
+    {
+        "name": "CaseOh",
+        "description": "Horror-focused content creator who specializes in atmospheric and indie horror walkthroughs and exploration-based experiences.",
+        "channel_link": "https://www.youtube.com/channel/UC63anZxfVGHUEmfBAf5w7pw",
+        "image_url": "images/CaseOh.webp",
+        "famous_horror_games_played": [
+            "Blood Wash", 
+            "The Coffin of Andy and Leyley", 
+            "The Last Monday", 
+            "Night Shift"
+        ]
+    },
+
+    {
+        "name": "8-BitRyan",
+        "description": "Gaming YouTuber well-known for his high-quality horror playthroughs, expressive reactions, and consistent support for indie horror developers.",
+        "channel_link": "https://www.youtube.com/@8BitRyan",
+        "image_url": "images/8BitRyan.webp",
+        "famous_horror_games_played": [
+            "Poppy Playtime", 
+            "The Joy of Creation", 
+            "Bendy and the Ink Machine", 
+            "Murder House"
+        ]
+    }
+]
