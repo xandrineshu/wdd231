4740dbe2c1c981a042f1c5c3d007f75bd6f1ae9b
xandrineshu <jconcha1@byupathway.edu>
2025-10-11 23:07:15 +0800

    yotubers section done

--- finalproject/data/youtubers.mjs
+++ finalproject/data/youtubers.mjs
@@ -1,10 +1,10 @@
 export const youtubers = [
     {
         "name": "KubzScouts",
-        "description": "Philippine-based YouTuber known for energetic horror playthroughs, jump scares, and late-night 'Free Random Horror Game' sessions.",
+        "description": "YouTuber known for energetic horror playthroughs, jump scares, and late-night 'Free Random Horror Game' sessions.",
         "channel_link": "https://www.youtube.com/channel/UCZBY6V8Lxmwu8gGRBOyO11w",
-        "image_url": "images/KubzScouts.webp",
-        "famous_horror_games_played": [
+        "image_url": "KubzScouts.webp",
+        "games_played": [
             "Free Random Horror Games", 
             "Silent Hill f", 
             "The Walking Dead", 
@@ -16,8 +16,8 @@ export const youtubers = [
         "name": "Markiplier",
         "description": "One of the most iconic horror gaming YouTubers, known for dramatic reactions, immersive commentary, and playthroughs of both indie and AAA horror titles.",
         "channel_link": "https://www.youtube.com/user/markiplierGAME",
-        "image_url": "images/Markiplier.webp",
-        "famous_horror_games_played": [
+        "image_url": "Markiplier.webp",
+        "games_played": [
             "Amnesia: The Dark Descent",
             "Five Nights at Freddy's series",
             "Outlast",
@@ -29,8 +29,8 @@ export const youtubers = [
         "name": "CoryxKenshin",
         "description": "Popular gaming YouTuber celebrated for his comedic horror playthroughs and his long-running 'Spooky Scary Sunday' horror series.",
         "channel_link": "https://www.youtube.com/c/CoryxKenshin",
-        "image_url": "images/CoryxKenshin.webp",
-        "famous_horror_games_played": [
+        "image_url": "CoryxKenshin.webp",
+        "games_played": [
             "Murder House", 
             "The Mortuary Assistant",
             "Stay Out of the House", 
@@ -42,8 +42,8 @@ export const youtubers = [
         "name": "CaseOh",
         "description": "Horror-focused content creator who specializes in atmospheric and indie horror walkthroughs and exploration-based experiences.",
         "channel_link": "https://www.youtube.com/channel/UC63anZxfVGHUEmfBAf5w7pw",
-        "image_url": "images/CaseOh.webp",
-        "famous_horror_games_played": [
+        "image_url": "CaseOh.webp",
+        "games_played": [
             "Blood Wash", 
             "The Mortuary Assistant", 
             "The Last Monday", 
@@ -55,8 +55,8 @@ export const youtubers = [
         "name": "8-BitRyan",
         "description": "Gaming YouTuber well-known for his high-quality horror playthroughs, expressive reactions, and consistent support for indie horror developers.",
         "channel_link": "https://www.youtube.com/@8BitRyan",
-        "image_url": "images/8BitRyan.webp",
-        "famous_horror_games_played": [
+        "image_url": "8-BitRyan.webp",
+        "games_played": [
             "Poppy Playtime", 
             "The Joy of Creation", 
             "Bendy and the Ink Machine", 
---
+++ finalproject/scripts/youtubers.js
@@ -0,0 +1,46 @@
+import { youtubers } from '../data/youtubers.mjs';
+
+const showHere = document.querySelector("#allyoutubers");
+
+function displayItems(youtubers) {
+    youtubers.forEach(x => {
+        const thecard = document.createElement('div');
+        thecard.classList.add('platform_card');
+
+        const thename = document.createElement('h2');
+        thename.innerText = x.name;
+        thecard.appendChild(thename);
+
+        const thephoto = document.createElement('img');
+        thephoto.src = `images/${x.image_url}`;
+        thephoto.alt = `Image of ${x.title}`;
+        thephoto.onerror = function () {
+            this.src = `https://placehold.co/400x225/1A1A25/E6E6E6?text=IMAGE+MISSING`;
+        };
+        thecard.appendChild(thephoto);
+
+        const thedesc = document.createElement('p');
+        thedesc.innerText = x.description;
+        thecard.appendChild(thedesc);
+
+        const thegames = document.createElement('ul');
+        thegames.innerText = `Popular Games Played: ${x.games_played}`;
+        thecard.appendChild(thegames);
+
+        const thelink = document.createElement('a');
+        thelink.textContent = '// Visit Channel';
+        thelink.href = x.channel_link;
+        thelink.target = "_blank";
+        thecard.appendChild(thelink);
+
+        showHere.appendChild(thecard);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (showHere) {
+        displayItems(youtubers);
+    } else {
+        console.error("Target element #allyoutubers not found.");
+    }
+});
