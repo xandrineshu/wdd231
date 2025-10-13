8f82f559283cf5c2cd16cfb92b9525aeee866572
xandrineshu <jconcha1@byupathway.edu>
2025-10-13 13:13:24 +0800

    update

--- finalproject/scripts/review.js
+++ finalproject/scripts/review.js
@@ -1,22 +1,3 @@
-// Hamburger
-
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
-});
-
-// Footer
-
-document.getElementById("currentyear").textContent =
-    new Date().getFullYear();
-document.getElementById(
-    "lastModified"
-).textContent = `Last Modified: ${document.lastModified}`;
-
-
 // Review Cards
 function toggleDossier(cardElement) {
     cardElement.classList.toggle('open');
